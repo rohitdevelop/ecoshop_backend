1994b6be48143ade4447586fa42e7ed865a5de91
Rohit singh <rohitdev124421@gmail.com>
2025-09-10 15:40:26 +0530

    eroor

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
-import connectDB from "./src/detabase/db.js"
+// import connectDB from "./src/detabase/db.js"
 import productRoutes from "./src/routes/productRoutes.js";
 import categoryRoutes from "./src/routes/catageryRoutes.js";
 import cors from "cors"
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-connectDB();
+// connectDB();
 
 // API routes
 app.use("/api/products", productRoutes);
